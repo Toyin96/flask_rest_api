286feaf8ae50de06bacd4c4aed7892d8bf8ebe11
Toyin96 <onagoruwam@gmail.com>
2021-05-16 23:11:13 +0100

    the rest api is now live

--- app.py
+++ app.py
@@ -4,33 +4,32 @@ from flask_marshmallow import Marshmallow
 
 import os
 
-# init app  
+#initializing our flask app
 app = Flask(__name__)
 
+# setting up our base directory
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-app.route("/", methods=["GET"])
-def hello():
-    return jsonify({"name": "Toyin"})
-
 # setting up our database
-app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'db.sqlite')
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///" + os.path.join(basedir, "db.sqlite")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-# init db
+
+# initialize our db
 db = SQLAlchemy(app)
 
-# init ma
+# initialize marshmallow
 ma = Marshmallow(app)
 
-#product class
+# creating our model is class product
 
-class product(db.Model):
+class Product(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(100), unique=True)
     description = db.Column(db.String(200))
     price = db.Column(db.Float)
     qty = db.Column(db.Integer)
+
 
     def __init__(self, name, description, price, qty):
         self.name = name
@@ -38,17 +37,53 @@ class product(db.Model):
         self.price = price
         self.qty = qty
 
-# product schema 
+# product schema
 class ProductSchema(ma.Schema):
     class Meta:
-        fields = ('id', 'name', 'description', 'qty')
-
-# init our schema
-product_schema = ProductSchema() # console would give us awarning if we don't do this strict stuff
-products_schema = ProductSchema(many=True) # many=True means we're dealing with more than 1 product    
+        fields = ('id', 'name', 'descrption', 'price', 'qty')
 
 
-# run server
+# let's initialize our schema
+product_schema = ProductSchema()
+products_schema = ProductSchema(many=True)
 
+
+# creating our routes
+# create a product
+
+@app.route("/product", methods=['POST'])
+def app_product():
+    name = request.json['name']
+    description = request.json['description']
+    price = request.json['price']
+    qty = request.json['qty']
+
+    new_product = Product(name, description, price, qty)
+
+    db.session.add(new_product)
+
+    # commiting/saving it to the database
+    db.session.commit()
+
+    return product_schema.jsonify(new_product)
+
+
+# Get all products
+@app.route("/products", methods=['GET'])
+def fetch_products():
+    all_products = Product.query.all()
+    results = products_schema.dump(all_products)
+    return jsonify(results)
+
+
+@app.route("/product/<id>", methods=['GET'])
+def fetch_product(id):
+    product = Product.query.get(id)
+    return product_schema.jsonify(product)    
+
+
+
+
+# run our server
 if __name__ == "__main__":
     app.run(debug=True)
