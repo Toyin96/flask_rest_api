7692777b28a845d2e3a62c71283fa3781665b3d4
Toyin96 <onagoruwam@gmail.com>
2021-07-18 12:29:28 +0100

    updated app.py

--- app.py
+++ app.py
@@ -81,11 +81,6 @@ def fetch_product(id):
     product = Product.query.get(id)
     return product_schema.jsonify(product)    
 
-@app.route("/product/<id>", methods=['GET'])
-def fetch_products(id):
-    product = Product.query.get(id)
-    return product_schema.jsonify(product)
-
 
 # run our server
 if __name__ == "__main__":
